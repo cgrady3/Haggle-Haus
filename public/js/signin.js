5614214d6594c726040d19d6b0c835eb96dff71d
Carolyn Grady <owenv8@gmail.com>
2020-03-30 14:58:23 -0400

    added sign in js

---
+++ public/js/signin.js
@@ -0,0 +1,69 @@
+var currentURL = window.location.origin;
+var userName;
+var password;
+var user;
+var signin = false;
+
+$("#login").click(function(event) {
+  event.preventDefault();
+  userName = $("#userName")
+    .val()
+    .trim();
+  password = $("#password")
+    .val()
+    .trim();
+  user = {
+    name: userName,
+    password: password
+  };
+  try {
+    $.get("/api/users", user).then(function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].name === user.name && data[i].password === user.password) {
+            user = data[i];
+            signin = true;
+        }
+      }
+      if (signin){
+        location.href = `currentURL${/home/user.id}`;
+      } else{
+        alert("Invalid user name of password");
+      }
+    });
+  } catch (err) {
+    if (ValidationError) {
+      alert("Invalid user name of password");
+    }
+  }
+  $("#userName").val("");
+  $("#password").val("");
+});
+
+$("#newUser").click(function(event) {
+  event.preventDefault();
+  userName = $("#userName")
+    .val()
+    .trim();
+  password = $("#password")
+    .val()
+    .trim();
+  user = {
+    name: userName,
+    password: password
+  };
+  try {
+    $.post("/api/users", user).then(function(data) {
+      console.log(data);
+      location.href = `currentURL${/home/data.id}`;
+    });
+  } catch (err) {
+    if (SequelizeUniqueConstraintError) {
+      alert("username is taken");
+    } else if (ValidationError) {
+      alert("Invalid user name of password");
+    }
+  }
+  $("#userName").val("");
+  $("#password").val("");
+});
